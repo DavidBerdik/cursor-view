a99780c36af4d949360ca28978d046ceab4988d8
David Berdik <berdikgit@theprogrammersworld.net>
2026-04-17 17:08:36 -0400

    Revert "Clean up orphaned temp files in the application's preferences directory."
    This reverts commit 0fb9212586c732557bbbee13f00a5e363d3bbed2.

--- server.py
+++ server.py
@@ -9,33 +9,8 @@ import threading
 import webbrowser
 
 from cursor_view.app_factory import create_app
-from cursor_view.paths import cursor_view_cache_dir
 
 logger = logging.getLogger(__name__)
-
-
-def _cleanup_orphan_temp_files() -> None:
-    """Remove ``chat-index.*.tmp*`` files left in the cache dir from prior
-    runs that were terminated mid-rebuild (see ``ChatIndex._rebuild``).
-
-    Sweeps only the top level of ``cursor_view_cache_dir()`` so that any
-    sibling subdirectories are untouched. Files held open by a
-    concurrently-running instance will fail to delete on Windows; those
-    errors are logged at debug level and skipped.
-    """
-    cache_dir = cursor_view_cache_dir()
-    removed = 0
-    for path in cache_dir.glob("chat-index.*.tmp*"):
-        try:
-            path.unlink()
-            removed += 1
-        except OSError:
-            logger.debug("Could not remove orphan temp file %s", path, exc_info=True)
-    if removed:
-        logger.info("Removed %d orphan temp file(s) from %s", removed, cache_dir)
-
-
-_cleanup_orphan_temp_files()
 
 app = create_app()
 
